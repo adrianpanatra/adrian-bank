7c8fa45af35463edb80af17670d32971b42ae89e
adrianpanatra <juliantoadrian@gmail.com>
2021-07-28 20:17:08 +0700

    adding DB call

--- app/app.go
+++ app/app.go
@@ -1,10 +1,11 @@
 package app
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/adrianpanatra/adrian-bank/domain"
+	"github.com/adrianpanatra/adrian-bank/service"
 	"github.com/gorilla/mux"
 )
 
@@ -13,22 +14,28 @@ func Start() {
 	// mux := http.NewServeMux()
 	router := mux.NewRouter()
 
-	// define routes
-	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
-	router.HandleFunc("/customers", getAllCustomer).Methods(http.MethodGet)
-	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	// wiring
+	// ch := CustomerHandlers{service.NewCustomerService((domain.NewCustomerRepositoryStub()))}
+	ch := CustomerHandlers{service.NewCustomerService((domain.NewCustomerRepositoryDB()))}
 
-	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+	// define routes
+	router.HandleFunc("/customers", ch.getAllCustomer).Methods(http.MethodGet)
+
+	// router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	// router.HandleFunc("/customers", getAllCustomer).Methods(http.MethodGet)
+	// router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+
+	// router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	// starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
 
-func getCustomer(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	fmt.Fprint(w, vars["customer_id"])
-}
+// func getCustomer(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	fmt.Fprint(w, vars["customer_id"])
+// }
 
-func createCustomer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Post request received")
-}
+// func createCustomer(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Post request received")
+// }
--- app/handler.go
+++ app/handler.go
@@ -3,8 +3,9 @@ package app
 import (
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"net/http"
+
+	"github.com/adrianpanatra/adrian-bank/service"
 )
 
 type Customer struct {
@@ -13,15 +14,17 @@ type Customer struct {
 	Zipcode string `json:"zip_code" xml:"zipcode"`
 }
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+// func greet(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Hello World")
+// }
+
+type CustomerHandlers struct {
+	service service.CustomerService
 }
 
-func getAllCustomer(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer{
-		{"Andi", "Jakarta", "10620"},
-		{"Budi", "Bandung", "30543"},
-	}
+func (ch *CustomerHandlers) getAllCustomer(w http.ResponseWriter, r *http.Request) {
+	customers, _ := ch.service.GetAllCustomer()
+
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Add("Content-Type", "application/xml")
 		xml.NewEncoder(w).Encode(customers)
