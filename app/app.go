1b63d26b4403340483e618c59781ade8cd9581d4
adrianpanatra <juliantoadrian@gmail.com>
2021-11-01 18:13:29 +0700

    assignment for transaction

--- app/app.go
+++ app/app.go
@@ -14,8 +14,8 @@ import (
 )
 
 func sanityCheck() {
-	if os.Getenv("SERVER_ADDRESS") == "" ||
-		os.Getenv("SERVER_PORT") == "" {
+	if os.Getenv("DB_ADDRESS") == "" ||
+		os.Getenv("DB_PORT") == "" {
 		log.Fatal("Environment variable not defined ....")
 	}
 }
@@ -37,6 +37,7 @@ func Start() {
 	router.HandleFunc("/customers", ch.getAllCustomer).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}/account", ah.NewAccount).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}/account/{account_id:[0-9]+}", ah.MakeTransaction).Methods(http.MethodPost)
 	router.HandleFunc("/customers{status}", ch.getCustomer).Methods(http.MethodGet)
 
 	// router.HandleFunc("/greet", greet).Methods(http.MethodGet)
